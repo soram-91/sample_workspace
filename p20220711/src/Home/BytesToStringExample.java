f39c5b7c27d1bdbcb5a9e827cfb9cde28880e462
user <user@125.137.40.211>
2022-07-11 23:16:00 +0900

    0711_home

--- p20220711/src/Home/BytesToStringExample.java
+++ p20220711/src/Home/BytesToStringExample.java
@@ -2,6 +2,10 @@ package Home;
 
 public class BytesToStringExample {
 	public static void main(String[] args) {
-		Byte[]bytes = {73,32,108,111,118,101,32,121,111,117};
+		byte[] bytes = { 73, 32, 108, 111, 118, 101, 32, 121, 111, 117 };
+		
+		String str = new String(bytes);
+						
+		System.out.println(str);
 	}
 }
---
+++ p20220711/src/Home/IntegerCompareExample.java
@@ -0,0 +1,14 @@
+package Home;
+
+public class IntegerCompareExample {
+	public static void main(String[] args) {
+		Integer obj1 = 100;
+		Integer obj2 = 100;
+		Integer obj3 = 300;
+		Integer obj4 = 400;
+		
+		System.out.println(obj1 == obj2);
+		System.out.println(obj3 == obj4);
+		
+	}
+}
