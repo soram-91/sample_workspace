e32cb6c3f1951515f9e639541fb0db146d97e1e8
user <user@210.97.23.247>
2022-07-26 20:46:23 +0900

    220726_home

--- homework/src/home0725/HomeW.java
+++ homework/src/home0725/HomeW.java
@@ -3,70 +3,50 @@ package home0725;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
-
+// 문제1) 다음은 키보드로부터 상품 수와 상품 가격을 입력받아서
+// 최고 가격을 가지는 제품과 해당 제품을 제외한 제품들의 총 합을 구하는 프로그램을 작성하세요.
+// 1) 메뉴는 다음과 같이 구성하세요.
+// 1.상품 수 | 2.상품 및 가격입력 | 3.제품별 가격 | 4.분석 | 5.종료
+// 2) 입력한 상품 수만큼 상품명과 해당 가격을 입력받을 수 있도록 구현하세요.
+// 3) 제품별 가격을 출력하세요.
+// 출력예시, "상품명 : 가격"
+// 4) 분석기능은 최고 가격을 가지는 제품과 해당 제품을 제외한 제품들의 총합을 구합니다.
+// 5) 종료 시에는 프로그램을 종료한다고 메세지를 출력하도록 구현하세요.
 public class HomeW {
 	public static void main(String[] args) {
 		Scanner scn = new Scanner(System.in);
-		
-		String[] list = null;
-		int [] priceList = null;
 		Store str = new Store();
+	
 		
 		boolean run = true;
 		while (run) {
-			System.out.println("1.상품 수 | 2.상품 및 가격입력 | 3.제품별 가격 | 4.분석 | 5.종료");
+			System.out.println("1.상품 및 가격입력 | 2.제품별 가격 | 3.분석 | 4.종료");
 			System.out.println("선택> ");
 			int selectNo = Integer.parseInt(scn.nextLine());
 
 			switch (selectNo) {
 
 			case 1:
-				System.out.println("상품 수를 입력하세요.");
-				int itemNo = Integer.parseInt(scn.nextLine());
-				list = new String[itemNo];
-				priceList = new int[itemNo];
+				System.out.println("제품명을 입력하세요");
+				String itemName = scn.nextLine();
+				str.setName(itemName);
+				System.out.println("제품의 가격을 입력하세요");
+				int price = Integer.parseInt(scn.nextLine());
+				str.setPrice(price);
 				break;
 			case 2:
-				for(int i =0; i < list.length; i++) {
-					System.out.println("상품을 입력하세요");
-					String name = scn.nextLine();
-					str.setName(name);					
-					System.out.println("가격을 입력하세요");
-					int price = Integer.parseInt(scn.nextLine());
-					str.setPrice(price);
-				}
-					
-
-					
-					
+				str.getName();
+				str.getPrice();
+				
 				break;
 			case 3:
-				for(int i = 0; i< list.length; i++) {
-					System.out.println("제품명: " + str.getName() +", 가격: " + str.getPrice());
-								
-				}
-
+				str.maxPriceItem();
+				str.sumPrice();
 				break;
 			case 4:
-				//4) 분석기능은 최고 가격을 가지는 제품과 해당 제품을 제외한 제품들의 총합을 구합니다.
-				int max = 0;
-				String maxItem = null;
-				int sum = 0;
-				int others = 0;
-				for(int i = 0; i< list.length; i++) {
-					if(str.getPrice() > max) {
-						max = str.getPrice();
-						maxItem = str.getName();
-					}
-					sum += str.getPrice();
-					others = sum-max;
-					}
-				
-				System.out.println("최고 가격을 가지는 제품 : " + maxItem + ", 해당 제품을 제외한 제품들의 총합 : " + others);
-				break;
-			case 5:
 				System.out.println("프로그램을 종료합니다.");
 				run = false;
+						
 			}
 		}
 		
--- homework/src/home0725/Store.java
+++ homework/src/home0725/Store.java
@@ -1,44 +1,81 @@
 package home0725;
 
+import java.util.Scanner;
+
 public class Store {
 	public int itemNo;
 	public String name;
 	public int price;
-	
+	String[] list = new String[10];
+	int[] priceList = new int[10];
+
+	Scanner scn = new Scanner(System.in);
+
 	public Store() {
-		
+
 	}
-	
+
 	public Store(int itemNo) {
 		this.itemNo = itemNo;
 	}
+
 	public Store(String name, int price) {
 		this.name = name;
 		this.price = price;
 	}
 
-	public String getName() {
-		return name;
+	public void getName() {
+
+		for (int i = 0; i < list.length; i++) {
+			list[i] = name;
+		}
+		System.out.print("제품명은 " + name + "이고, ");
+
+	}
+
+	public void setPrice(int price) {
+		this.price = price;
 	}
 
 	public void setName(String name) {
 		this.name = name;
 	}
 
-	public int getPrice() {
-		return price;
+	public void getPrice() {
+		for (int i = 0; i < priceList.length; i++) {
+			priceList[i] = price;
+		}
+		System.out.print("가격은 " + price + "입니다\n");
+
 	}
 
-	public void setPrice(int price) {
-		this.price = price;
+	public void maxPriceItem() {
+		int max = 0;
+		String maxPriceItem = null;
+		for (int i = 0; i < priceList.length; i++) {
+			for (int j = 0; j < list.length; j++) {
+				if (priceList[i] > max) {
+					max = priceList[i];
+					maxPriceItem = list[j];
+				}
+			}
+		}
+		System.out.print("최고 가격 제품은 " + maxPriceItem + "이고, ");
 	}
-	
-//	public void maxPriceItem() {
-//		
-//		for(int i =0; i<list.length; i++) {
-//			if()
-//		}
-//	}
-	
-	
+
+	public void sumPrice() {
+		int sum = 0;
+		int max = 0;
+		int sumPrice = 0;
+		for (int i = 0; i < priceList.length; i++) {
+			if (priceList[i] > max) {
+				max = priceList[i];
+			}
+			sum += priceList[i];
+		}
+		sumPrice = sum - max;
+		System.out.print("나머지 물품의 합계는 : " + sumPrice + "입니다. \n");
+
+	}
+
 }
